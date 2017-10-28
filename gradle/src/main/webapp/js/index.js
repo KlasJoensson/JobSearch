c4e86ade45c10ed47458f6b1bbe2af11b65c5e68
Klas Jönsson <klas.joensson@gmail.com>
2017-10-28 19:24:56 +0200

    Handles the case with no adds in the search result

--- gradle/src/main/webapp/js/index.js
+++ gradle/src/main/webapp/js/index.js
@@ -5,17 +5,21 @@ function search() {
     $.post("counties.jsp", {keyWords: $("#searchFor").val() },
         function(data) {
             var adds = data.matchningslista.matchningdata;
-            var rows = '<tr><th>Rubrik</th><th>Företag</th><th>Publicerad</th><th>Sök senast</th><th>Relevans</th></tr>';
-            $.each(adds, function(index, add) {
-                var row = '<tr>';
-                row += '<td>' + add.annonsrubrik  + '</td>';
-                row += '<td>' + add.arbetsplatsnamn + '</td>';
-                row += '<td>' + add.publiceraddatum.split('T')[0] + '</td>';
-                row += '<td>' + add.sista_ansokningsdag.split('T')[0] + '</td>';
-                row += '<td>' + add.relevans + '</td>';
-                rows += row + '<tr>';
-            });
-            $('#result').html(rows);
-        });
-
+            if (data.matchningslista.antal_platserTotal > 0) {
+                var rows = '<tr><th>Rubrik</th><th>Företag</th><th>Publicerad</th><th>Sök senast</th><th>Relevans</th></tr>';
+                $.each(adds, function (index, add) {
+                    var row = '<tr>';
+                    row += '<td>' + add.annonsrubrik + '</td>';
+                    row += '<td>' + add.arbetsplatsnamn + '</td>';
+                    row += '<td>' + add.publiceraddatum.split('T')[0] + '</td>';
+                    row += '<td>' + add.sista_ansokningsdag.split('T')[0] + '</td>';
+                    row += '<td>' + add.relevans + '</td>';
+                    rows += row + '<tr>';
+                });
+                $('#result').html(rows);
+            } else {
+                alert("Hittade inga annonser!")
+            }
+        }
+    );
 }
