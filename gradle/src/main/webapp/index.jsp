7061548f67677f87c92b3204ee58fef241804068
Klas Jönsson <klas.joensson@gmail.com>
2017-10-28 18:23:10 +0200

    Adds the search result to a simple table also refactors the JavaScript

--- gradle/src/main/webapp/index.jsp
+++ gradle/src/main/webapp/index.jsp
@@ -8,17 +8,9 @@
 This will be a web app for searching and rating job adds provided by the API from Arbetsförmedlingen.
 <br>
 Sök efter: <input type="text" id="searchFor"><br>
-<button id="test-btn" onclick="test()">Sök</button>
-
-<script>
-function test() {
-    $.post("counties.jsp", {keyWords: $("#searchFor").val() },
-        function(data) {
-            alert("Antal hittade anonnser: " + data.matchningslista.antal_platserTotal);
-    });
-
-}
-</script>
+<button id="test-btn" onclick="search()">Sök</button>
+<table id="result" border="1px solid black"></table>
+<script src="js/index.js"></script>
 </body>
 
 </html>
---
+++ gradle/src/main/webapp/js/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by klas on 2017-10-28.
+ */
+function search() {
+    $.post("counties.jsp", {keyWords: $("#searchFor").val() },
+        function(data) {
+            var adds = data.matchningslista.matchningdata;
+            var rows = '<tr><th>Rubrik</th><th>Företag</th><th>Publicerad</th><th>Sök senast</th><th>Relevans</th></tr>';
+            $.each(adds, function(index, add) {
+                var row = '<tr>';
+                row += '<td>' + add.annonsrubrik  + '</td>';
+                row += '<td>' + add.arbetsplatsnamn + '</td>';
+                row += '<td>' + add.publiceraddatum.split('T')[0] + '</td>';
+                row += '<td>' + add.sista_ansokningsdag.split('T')[0] + '</td>';
+                row += '<td>' + add.relevans + '</td>';
+                rows += row + '<tr>';
+            });
+            $('#result').html(rows);
+        });
+
+}
